7b8bce9d64d1d2cd2ce320816261391bfde477e2
Žiga Maj Špiler <zmspiler@pm.me>
2025-09-27 18:06:09 +0200

    Set up mantine

--- inventory-management/src/main.tsx
+++ inventory-management/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "@mantine/core/styles.css";
+import "./index.css";
+import App from "./App.tsx";
+import { createTheme, MantineProvider } from "@mantine/core";
+
+const theme = createTheme({});
+
+// biome-ignore lint/style/noNonNullAssertion: Default react app code
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<MantineProvider theme={theme}>
+			<App />
+		</MantineProvider>
+	</StrictMode>,
+);
