03d5df9433a5b0e87a9c9c35dfa9217bceaf61b2
Žiga Maj Špiler <zmspiler@pm.me>
2025-09-28 17:49:31 +0200

    Add turborepo

--- inventory-management/web/src/components/navbar/navbar.tsx
+++ inventory-management/web/src/components/navbar/navbar.tsx
@@ -1,57 +1,46 @@
 import { Flex, NavLink, Stack, type NavLinkProps } from "@mantine/core";
 import { Link } from "@tanstack/react-router";
 import {
-  IconBuildingWarehouse,
-  IconLayoutDashboard,
-  IconLogout,
+	IconBuildingWarehouse,
+	IconLayoutDashboard,
+	IconLogout,
 } from "@tabler/icons-react";
-import { supabase } from "../../db/supabase";
 
 export function Navbar() {
-  const response = supabase
-    .from("user_info")
-    .select(`
-    organizations (
-      id,
-      name
-    )
-  `)
-    .single();
-
-  return (
-    <Stack justify="space-between" h={"100%"}>
-      <div>
-        <Flex></Flex>
-        <StyledLink
-          leftSection={<IconLayoutDashboard size={16} />}
-          label="Dashboard"
-          to="/dashboard"
-        />
-        <StyledLink
-          leftSection={<IconBuildingWarehouse size={16} />}
-          label="Inventory"
-          to="/inventory"
-        />
-      </div>
-      <div>
-        <StyledLink
-          leftSection={<IconLogout size={16} />}
-          label="Log Out"
-          to="/logout"
-        />
-      </div>
-    </Stack>
-  );
+	return (
+		<Stack justify="space-between" h={"100%"}>
+			<div>
+				<Flex></Flex>
+				<StyledLink
+					leftSection={<IconLayoutDashboard size={16} />}
+					label="Dashboard"
+					to="/dashboard"
+				/>
+				<StyledLink
+					leftSection={<IconBuildingWarehouse size={16} />}
+					label="Inventory"
+					to="/inventory"
+				/>
+			</div>
+			<div>
+				<StyledLink
+					leftSection={<IconLogout size={16} />}
+					label="Log Out"
+					to="/logout"
+				/>
+			</div>
+		</Stack>
+	);
 }
 
 const StyledLink = (props: NavLinkProps & { to: string }) => {
-  return (
-    <NavLink
-      style={{
-        borderRadius: "var(--mantine-radius-sm)",
-      }}
-      component={Link}
-      {...props}
-    />
-  );
+	return (
+		<NavLink
+			style={{
+				borderRadius: "var(--mantine-radius-sm)",
+			}}
+			component={Link}
+			{...props}
+		/>
+	);
 };
