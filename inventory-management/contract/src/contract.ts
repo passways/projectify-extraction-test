df01326942dbf52d8281a63dd682ca16d2659cc0
Žiga Maj Špiler <zmspiler@pm.me>
2025-10-07 10:14:12 +0200

    Add setup procedure

--- inventory-management/contract/src/contract.ts
+++ inventory-management/contract/src/contract.ts
@@ -1,3 +1,5 @@
+import { oc } from "@orpc/contract";
+import z from "zod";
 import { inventoryContract } from "./contracts/inventory";
 import { locationContract } from "./contracts/location";
 import { sessionContract } from "./contracts/session";
@@ -6,4 +8,18 @@ export const contract = {
   session: sessionContract,
   location: locationContract,
   inventory: inventoryContract,
+  setup: oc
+    .input(
+      z.object({
+        name: z.string().trim().min(1),
+        email: z.email(),
+        password: z.string().trim().min(8),
+      }),
+    )
+    .errors({
+      INTERNAL_SERVER_ERROR: {
+        status: 500,
+        description: "Failed to create user",
+      },
+    }),
 };
